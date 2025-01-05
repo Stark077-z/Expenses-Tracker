2d8c7774037a565e189896b3ada67a34c0e1f24b
Salah eddine hadraoui <hadraouisalah8@gmail.com>
2025-01-06 00:53:49 +0100

    adjucted the regExp for the description form
    this change is for accepting the spaces in the discreption text area

--- src/Global data/data.js
+++ src/Global data/data.js
@@ -1,6 +1,6 @@
 export const categories =['Other', 'Food' , 'Transport' , 'Entertainment' , 'Shopping' , 'Bills']
 
-export const OnlyStringChars=/^[a-zA-Z]+$/;
+export const OnlyStringChars=/^[a-zA-Z\s]+$/;
 export const OnlyNumbers=/^\d+(\.\d+)?$/;
 export const handleDateTime =(date) =>{
     const Months = [
